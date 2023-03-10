444de5c247de14e3f36cdb62cb781d30917cd654
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-03-10 13:51:15 +0000

    changes with nav responsiveness

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Home from "./pages/home";
+import Header from "./components/header";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Header />
     </div>
   );
 }
