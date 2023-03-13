49605515f0b07e0f1c4ecd535fa211ce479c7793
Agustin Marani Ghiacci <agusnicolas21@gmail.com>
2023-03-13 10:11:16 +0000

    Some styles config

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,11 +1,36 @@
 import axios from "axios"
-
+import { Button, Checkbox, Form, Input, Space } from 'antd';
 
 export const Login = () => {
+    const onFinish = (values) => {
+        // console.log('Success:', values);
+        // userLog.username = values.username;
+        // setValue(() => !value)
+        // navigate("/home")
+    };
+
+    const onFinishFailed = (errorInfo) => {
+        // console.log('Failed:', errorInfo);
+    };
 
     return (
         <>
-            <h1>hi?</h1>
+            <Space direction="vertical" size="middle" align="center" style={{ display: 'flex' }}>
+                <Form name="basic" labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed} autoComplete="off">
+                    <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+                        <Input.Password />
+                    </Form.Item>
+                    <Form.Item wrapperCol={{ offset: 1, span: 10 }}>
+                        <Button className="submit-btn" type="primary" htmlType="submit" size="large">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Space>
         </>
     )
 }
