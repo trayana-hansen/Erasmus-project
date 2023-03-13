66716f321fca1951852d1ab9129c65734a08ec2b
Agustin Marani Ghiacci <agusnicolas21@gmail.com>
2023-03-13 14:08:45 +0000

    cors

--- API/server.js
+++ API/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors"
 import routeRoutes from "./routes/route.js";
 import routeCities from "./routes/city.js";
 import routerUser from "./routes/user.js";
@@ -11,15 +12,26 @@ import routeImages from "./routes/image.js";
 
 const app = express();
 
+let corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(routeImages);
 app.use(routerUser);
-app.use(verifyAccessToken.bind(), routeRoutes);
-app.use(verifyAccessToken.bind(), routeCities);
-app.use(verifyAccessToken.bind(), routeMonuments);
-app.use(verifyAccessToken.bind(), routeProgresses);
-app.use(verifyAccessToken.bind(), routeReviews);
-app.use(verifyAccessToken.bind(), routeRiddles);
+app.use(routeRoutes);
+app.use(routeCities);
+app.use(routeMonuments);
+app.use(routeProgresses);
+app.use(routeReviews);
+app.use(routeRiddles);
+// app.use(verifyAccessToken.bind(), routeRoutes);
+// app.use(verifyAccessToken.bind(), routeCities);
+// app.use(verifyAccessToken.bind(), routeMonuments);
+// app.use(verifyAccessToken.bind(), routeProgresses);
+// app.use(verifyAccessToken.bind(), routeReviews);
+// app.use(verifyAccessToken.bind(), routeRiddles);
 
 export default app;
