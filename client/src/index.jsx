1e524d6720999963c00d2aa5ae2ce413f681b4d6
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-03-13 16:11:46 +0000

    home page styling

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ConfigProvider } from 'antd';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ConfigProvider } from "antd";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ConfigProvider
-    theme={{
-      token: {
-        colorPrimary: '#00b96b',
-      },
-    }}
-  >
-    <App />
+      theme={{
+        token: {},
+      }}
+    >
+      <App />
     </ConfigProvider>
   </React.StrictMode>
 );
--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,30 +1,42 @@
-import { Player } from '@lottiefiles/react-lottie-player';
-import { Button, Space } from 'antd';
+import { Player } from "@lottiefiles/react-lottie-player";
+import { Button, Space } from "antd";
 import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
   const navigate = useNavigate();
 
   const goTo = (dest) => {
-    console.log(dest)
-    navigate(`/${dest}`)
-  }
+    console.log(dest);
+    navigate(`/${dest}`);
+  };
 
   return (
     <>
-      <h1 className="homeHeading">Riddlin</h1>
-      <Player
-        src='https://assets7.lottiefiles.com/packages/lf20_svy4ivvy.json'
-        className="player" loop
-        autoplay
-        speed={1}
-      />
+      <div className="backgroundHome">
+        <h1 className="homeHeading">Riddlin</h1>
+        <Player
+          src="https://assets7.lottiefiles.com/packages/lf20_svy4ivvy.json"
+          className="player"
+          loop
+          autoplay
+          speed={1}
+        />
 
-      <Space direction="vertical" size="middle" align="center" style={{ display: 'flex' }} className="buttonWrapper">
-        <Button className='home-btn' onClick={() => goTo('login')} >LOGIN</Button>
-        <Button className='home-btn' onClick={() => goTo("register")} >SIGN UP</Button>
-      </Space>
-    </>);
+        <Space
+          direction="vertical"
+          size="middle"
+          align="center"
+          style={{ display: "flex" }}
+          className="buttonWrapper"
+        >
+          <Button className="home-btn-r" onClick={() => goTo("register")}>
+            SIGN UP
+          </Button>
+          <Button className="home-btn-s" onClick={() => goTo("login")}>
+            LOGIN
+          </Button>
+        </Space>
+      </div>
+    </>
+  );
 };
-
-
