9f82493ce169dfb7e1732ef0c81511b3e0be69b2
davidcasimiro77 <casimiroherreradavid2@gmail.com>
2023-03-13 10:06:00 +0000

    cityList

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Login } from "./pages/login";
+import Cities from "./pages/cityList";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={< Login />} />
-        <Route path="/" element={< Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cityList" element={<Cities />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ client/src/pages/cityList.jsx
@@ -0,0 +1,13 @@
+import { Card } from "antd";
+const Cities = () => (
+  <Card
+    style={{
+      backgroundImage:
+        "url('https://i.blogs.es/91493f/sequelize/1366_2000.png')",
+      width: 300,
+    }}
+  >
+    <h1>City's name</h1>
+  </Card>
+);
+export default Cities;
