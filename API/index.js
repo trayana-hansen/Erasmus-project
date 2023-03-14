90a72f061075ba882fe75eb1321faf5ca3245116
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-03-14 11:13:42 +0000

    trayana setup

--- API/index.js
+++ API/index.js
@@ -1,24 +1,23 @@
-import app from './server.js'
-import { sequelize } from './config/db.js'
-import dotenv from 'dotenv'
-import './models/riddle.js'
-import './models/route.js'
-import './models/monument.js'
-import './models/city.js'
-import './models/review.js'
-import './models/user.js'
-import './models/progress.js'
+import app from "./server.js";
+import { sequelize } from "./config/db.js";
+import dotenv from "dotenv";
+import "./models/riddle.js";
+import "./models/route.js";
+import "./models/monument.js";
+import "./models/city.js";
+import "./models/review.js";
+import "./models/user.js";
+import "./models/progress.js";
 
-dotenv.config()
+dotenv.config();
 async function main() {
-    try {
-        await sequelize.sync()
-        app.listen(process.env.SERVER_PORT)
-        console.log("Server Running")
-    }
-    catch {
-        console.log("Unable to connect to Database")
-    }
+  try {
+    await sequelize.sync({ force: true }  );
+    app.listen(process.env.SERVER_PORT);
+    console.log("Server Running");
+  } catch {
+    console.log("Unable to connect to Database");
+  }
 }
 
-main()
+main();
