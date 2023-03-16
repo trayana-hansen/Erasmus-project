5fec153a44ae79444058bb3a03424ff65b3cc150
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-03-16 08:53:14 +0000

    route page with card component

--- client/src/pages/route.jsx
+++ client/src/pages/route.jsx
@@ -1,61 +1,33 @@
-import { Card, Carousel, Space, Rate, Image } from "antd";
-import { useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
 import { Bground } from "../components/bground";
-import axios from "axios";
+import Header from "../components/header";
+import { CardRoute } from "../components/card";
+import { Modal } from "antd";
+import { useState } from "react";
 
-export const RoutePath = () => {
-  const [routes, setRoutes] = useState([]);
-
-  const onChange = (currentSlide) => {
-    console.log(currentSlide);
+export const RouteInfo = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
   };
-
-  const reviews = "http://localhost:4000/dl/reviews";
-  const desc = "`${reviews.grade}`";
-  const [value, setValue] = useState(3);
-
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <>
       <Bground />
-      <Space
-        direction="vertical"
-        size="middle"
-        align="center"
-        style={{ display: "flex" }}
-        className="route-container"
+      <Header />
+      <CardRoute onClick={showModal} />
+      <Modal
+        title="Basic Modal"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
       >
-        <h2 className="routeHeading">{routes.name}</h2>
-        <Carousel afterChange={onChange}>
-          <div>
-            {routes.length > 0
-              ? routes.map((route) => {
-                  return (
-                    <Card className="route-card">
-                      <Image src={`${route.img}`} /> //TODO are we hardcoding
-                      the img of the route or putting it in the database?
-                      <p>{routes.description}</p>
-                      <span>
-                        <Rate
-                          tooltips={desc}
-                          onChange={setValue}
-                          value={value}
-                        />
-                        {value ? (
-                          <span className="ant-rate-text">
-                            {desc[value - 1]}
-                          </span>
-                        ) : (
-                          ""
-                        )}
-                      </span>
-                    </Card>
-                  );
-                })
-              : null}
-          </div>
-        </Carousel>
-      </Space>
+        <img src={require("../assets/img/cities/arucas.png")} alt="" />
+      </Modal>
     </>
   );
 };
